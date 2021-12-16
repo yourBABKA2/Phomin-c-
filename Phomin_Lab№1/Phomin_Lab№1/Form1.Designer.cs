2285a3732c480b9869eaebca761ea2a063995da0
yourBABKA2 <72077298+yourBABKA2@users.noreply.github.com>
2021-12-16 22:59:26 +0300

    Add files via upload

---
+++ Phomin_Lab№1/Phomin_Lab№1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Phomin_Lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.comboBox_Figures = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxDraw = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDraw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonDelete);
+            this.panel1.Controls.Add(this.buttonAdd);
+            this.panel1.Controls.Add(this.comboBox_Figures);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(547, 74);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDelete.Location = new System.Drawing.Point(365, 27);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(96, 32);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAdd.Location = new System.Drawing.Point(263, 27);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(96, 32);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // comboBox_Figures
+            // 
+            this.comboBox_Figures.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox_Figures.FormattingEnabled = true;
+            this.comboBox_Figures.Items.AddRange(new object[] {
+            "Circle",
+            "Square"});
+            this.comboBox_Figures.Location = new System.Drawing.Point(104, 30);
+            this.comboBox_Figures.Name = "comboBox_Figures";
+            this.comboBox_Figures.Size = new System.Drawing.Size(142, 28);
+            this.comboBox_Figures.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(29, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Figure:";
+            // 
+            // pictureBoxDraw
+            // 
+            this.pictureBoxDraw.Location = new System.Drawing.Point(-1, 65);
+            this.pictureBoxDraw.Name = "pictureBoxDraw";
+            this.pictureBoxDraw.Size = new System.Drawing.Size(547, 482);
+            this.pictureBoxDraw.TabIndex = 1;
+            this.pictureBoxDraw.TabStop = false;
+            this.pictureBoxDraw.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBoxDraw_Paint);
+            this.pictureBoxDraw.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxDraw_MouseDown);
+            this.pictureBoxDraw.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxDraw_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 545);
+            this.Controls.Add(this.pictureBoxDraw);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDraw)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.ComboBox comboBox_Figures;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxDraw;
+    }
+}
+
---
+++ Phomin_Lab№1/Phomin_Lab№1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phomin_Lab_1
+{
+    public partial class Form1 : Form
+    {
+        const int padding = 16;
+
+        int old_x, old_y;
+
+        List<Figure> lst = new List<Figure>();
+
+        Figure createFigure(string fig_type)
+        {
+            switch (fig_type)
+            {
+                case "Circle": return new Circle();
+                case "Square": return new Square();
+            }
+            return null;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxDraw_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.White, 0, 0, pictureBoxDraw.Width, pictureBoxDraw.Height);
+            foreach (Figure fig in lst)
+                fig.draw(e.Graphics);
+        }
+
+        private void pictureBoxDraw_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int dx = e.X - old_x;
+                int dy = e.Y - old_y;
+                foreach (Figure fig in lst)
+                {
+                    if (fig.selected == false) continue;
+                    fig.pos_x += dx;
+                    fig.pos_y += dy;
+                }
+                pictureBoxDraw.Invalidate();
+            }
+            old_x = e.X;
+            old_y = e.Y;
+        }
+
+        private void pictureBoxDraw_MouseDown(object sender, MouseEventArgs e)
+        {
+            foreach (Figure fig in lst)
+                fig.selected = false;
+            for (int i = lst.Count - 1; i >= 0; i--)
+            {
+                Figure fig = lst[i];
+                fig.selected |= fig.test(e.X, e.Y);
+                if (fig.selected == true) break;
+            }
+            pictureBoxDraw.Invalidate();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i < lst.Count)
+            {
+                if (lst[i].selected == true) lst.RemoveAt(i);
+                i++;
+            }
+            pictureBoxDraw.Invalidate();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            Figure fig = createFigure(comboBox_Figures.Text);
+            if (fig == null) return;
+            fig.pos_x = 100.0f;
+            fig.pos_y = 100.0f;
+            lst.Add(fig);
+            pictureBoxDraw.Invalidate();
+        }
+
+    }
+
+    public class Figure
+    {
+        public float pos_x, pos_y;
+        public bool selected;
+
+        virtual public bool test(float x, float y)
+        {
+            return false;
+        }
+
+        virtual public void draw(Graphics g)
+        {
+
+        }
+    }
+
+    public class Circle : Figure
+    {
+        public float radius;
+
+        public Circle()
+        {
+            radius = 50.0f;
+        }
+
+        public override bool test(float x, float y)
+        {
+            float radius_square = radius * radius;
+            float dx = x - pos_x;
+            float dy = y - pos_y;
+            if (dx * dx + dy * dy <= radius_square) return true;
+            return false;
+        }
+
+        public override void draw(Graphics g)
+        {
+            float diameter = radius * 2.0f;
+            float x0 = pos_x - radius;
+            float y0 = pos_y - radius;
+            Pen p = Pens.Black;
+            if (selected == true) p = Pens.Red;
+            g.DrawEllipse(p, x0, y0, diameter, diameter);
+        }
+    }
+
+    public class Square : Figure
+    {
+        public float side;
+
+        public Square()
+        {
+            side = 100;
+        }
+
+        public override bool test(float x, float y)
+        {
+            float half_side = side * 0.5f;
+            float xmin = pos_x - half_side;
+            float ymin = pos_y - half_side;
+            float xmax = pos_x + half_side;
+            float ymax = pos_y + half_side;
+            if (x < xmin || y < ymin) return false;
+            if (x > xmax || y > ymax) return false;
+            return true;
+        }
+
+        public override void draw(Graphics g)
+        {
+            float half_side = side * 0.5f;
+            float x0 = pos_x - half_side;
+            float y0 = pos_y - half_side;
+            Pen p = Pens.Black;
+            if (selected == true) p = Pens.Red;
+            g.DrawRectangle(p, x0, y0, side, side);
+        }
+    }
+}
